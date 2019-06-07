ae24ca07d01dba4311f7bece524c8b8bd6f94563
WyattO <wto5568@gmail.com>
2019-06-06 18:19:42 -0700

    new new

---
+++ code.js
@@ -0,0 +1,10 @@
+
+
+
+      function scrollFunction() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbar-top").style.padding = "4px 10px";
+      } else {
+        document.getElementById("navbar-top").style.padding = "16px 10px";
+      }
+    }        
